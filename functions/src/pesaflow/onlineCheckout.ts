88a8fc8201c2f9745dad880080d33edd8e823e5a
Breldan-Muturi <breldan@sohnandsol.com>
2023-12-29 20:16:32 +0300

    Logging functions

--- functions/src/pesaflow/onlineCheckout.ts
+++ functions/src/pesaflow/onlineCheckout.ts
@@ -28,13 +28,52 @@ export async function onlineCheckout(req: Request, res: Response) {
 
   const key = "dTaI5iILm82p5Frc";
 
+  logger.debug("dataString: ", dataString);
+
   const hmacHash = (key: string, dataString: string) => {
     let hmac = createHmac("sha256", key);
-    let signed = hmac.update(Buffer.from(dataString, "utf-8")).digest("hex");
-    return signed;
+    let signed = hmac.update(Buffer.from(dataString, "utf-8")).digest();
+    logger.debug("Typescript Base64 Hash: ", signed.toString("hex"));
+    return signed.toString("base64");
   };
 
   const secureHash = hmacHash(key, dataString);
+  logger.debug("secureHash: ", secureHash);
+
+  const testDataString =
+    "133" +
+    "1" +
+    "48798" +
+    "11111111" +
+    "KES" +
+    "SUP_3361" +
+    "Some bill description" +
+    "John Doe" +
+    "XV7N7p2fh9GPKf4Wv2RE3S1T0Vrv44dj";
+  logger.debug("testDataString: ", testDataString);
+
+  const testSecureHash = () => {
+    let hmac = createHmac("sha256", "dTaI5iILm82p5Frc");
+    let testSigned = hmac
+      .update(
+        Buffer.from(
+          "133" +
+            "1" +
+            "48798" +
+            "11111111" +
+            "KES" +
+            "SUP_3361" +
+            "Some bill description" +
+            "John Doe" +
+            "XV7N7p2fh9GPKf4Wv2RE3S1T0Vrv44dj",
+          "utf-8"
+        )
+      )
+      .digest();
+    logger.debug("Test Typescript Base64 Hash: ", testSigned.toString("hex"));
+    return testSigned.toString("base64");
+  };
+  logger.debug("testSecureHash() :", testSecureHash());
 
   axios({
     method: "POST",
@@ -43,8 +82,9 @@ export async function onlineCheckout(req: Request, res: Response) {
       "Content-Type": "application/json",
     },
     data: {
-      "secureHash":
-        "ZTllODg5MjMwNGNlNWZjOTRjNDI2ZmZmODA4OTJhNWM5NjM3Mzc3YjYzYzhhNDM4ZTUyYjMxZDJhNGVjMzA2MQ==",
+      // "secureHash":
+      //   "ZTllODg5MjMwNGNlNWZjOTRjNDI2ZmZmODA4OTJhNWM5NjM3Mzc3YjYzYzhhNDM4ZTUyYjMxZDJhNGVjMzA2MQ==",
+      "secureHash": testSecureHash(),
       "apiClientID": apiClientID,
       "serviceID": serviceID,
       "notificationURL":
@@ -64,20 +104,13 @@ export async function onlineCheckout(req: Request, res: Response) {
     },
   })
     .then((response: any) => {
-      logger.info("secureHash:", secureHash);
-      res.status(200).json(response.data);
-      if (
-        response.data.status === 422 &&
-        response.data.message === "Invoice already paid"
-      ) {
-        return res.status(422).json(response.data);
-      } else {
-        return res.status(200).json(response.data);
-      }
+      res.status(response.status).json(response.data);
     })
     .catch((error: any) => {
       logger.error(error);
       logger.info("secureHash:", secureHash);
-      res.status(401).json({error});
+      res
+        .status(error.response?.status || 500)
+        .json(error.response?.data || {error: "An unknown error occurred"});
     });
 }
