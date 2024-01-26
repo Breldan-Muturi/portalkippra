077f8eb87d38974cfa8314a1496b52ad99845741
Breldan-Muturi <breldan@sohnandsol.com>
2024-01-26 19:46:01 +0300

    Updates the online checkout to fetch the payment from the portal

--- functions/src/pesaflow/onlineCheckout.ts
+++ functions/src/pesaflow/onlineCheckout.ts
@@ -2,22 +2,55 @@ import type {Request, Response} from "express";
 import * as logger from "firebase-functions/logger";
 import axios from "axios";
 import {createHmac} from "crypto";
+import {ConversionUtils} from "turbocommons-ts";
 
 export async function onlineCheckout(req: Request, res: Response) {
-  const url = "https://test.pesaflow.com/PaymentAPI/iframev2.1.php";
-  const serviceID = "48798";
-  const apiClientID = "133";
-  const amount = "1";
-  const clientIDNumber = "11111111";
-  const currency = "KES";
-  const billRefNumber = "SUP_3361";
-  const billDesc = "Some bill description";
-  const clientName = "John Doe";
-  const secret = "XV7N7p2fh9GPKf4Wv2RE3S1T0Vrv44dj";
+  const {
+    body: {
+      url,
+      serviceID,
+      apiClientID,
+      secret,
+      amountExpected,
+      clientIDNumber,
+      currency,
+      billRefNumber,
+      billDesc,
+      callBackURLOnSuccess,
+      clientName,
+      pictureURL,
+      sendSTK,
+      clientMSISDN,
+      clientEmail,
+      key,
+      format,
+      notificationURL,
+    },
+  } = req;
+  logger.debug("Request body: ", req.body);
+  // const amountExpected: "1"
+  // const apiClientID: "133"
+  // const billDesc: "Some bill description"
+  // const billRefNumber: "SUP_3361"
+  // const callBackURLOnSuccess: "google.com"
+  // const clientIDNumber: "11111111"
+  // const clientMSISDN: "25472"
+  // const clientName: "John Doe"
+  // const currency: "KES"
+  // const format: "json"
+  // const key: "dTaI5iILm82p5Frc"
+  // const notificationURL =
+  //   "https://webhook.site/bfb17e79-a804-4fd9-9478-e9ad80cb8111";
+  // const secret: "XV7N7p2fh9GPKf4Wv2RE3S1T0Vrv44dj";
+  // const sendSTK: true;
+  // const serviceID: "48798";
+  // const url: "https://test.pesaflow.com/PaymentAPI/iframev2.1.php";
+  // const pictureURL: null;
+  // const clientEmail: "john.doe@example.com";
 
   const dataString =
     apiClientID +
-    amount +
+    amountExpected +
     serviceID +
     clientIDNumber +
     currency +
@@ -26,54 +59,17 @@ export async function onlineCheckout(req: Request, res: Response) {
     clientName +
     secret;
 
-  const key = "dTaI5iILm82p5Frc";
-
   logger.debug("dataString: ", dataString);
 
   const hmacHash = (key: string, dataString: string) => {
     let hmac = createHmac("sha256", key);
     let signed = hmac.update(Buffer.from(dataString, "utf-8")).digest();
-    logger.debug("Typescript Base64 Hash: ", signed.toString("hex"));
-    return signed.toString("base64");
+    let secureValue = ConversionUtils.stringToBase64(signed.toString("hex"));
+    return secureValue;
   };
 
   const secureHash = hmacHash(key, dataString);
   logger.debug("secureHash: ", secureHash);
-
-  const testDataString =
-    "133" +
-    "1" +
-    "48798" +
-    "11111111" +
-    "KES" +
-    "SUP_3361" +
-    "Some bill description" +
-    "John Doe" +
-    "XV7N7p2fh9GPKf4Wv2RE3S1T0Vrv44dj";
-  logger.debug("testDataString: ", testDataString);
-
-  const testSecureHash = () => {
-    let hmac = createHmac("sha256", "dTaI5iILm82p5Frc");
-    let testSigned = hmac
-      .update(
-        Buffer.from(
-          "133" +
-            "1" +
-            "48798" +
-            "11111111" +
-            "KES" +
-            "SUP_3361" +
-            "Some bill description" +
-            "John Doe" +
-            "XV7N7p2fh9GPKf4Wv2RE3S1T0Vrv44dj",
-          "utf-8"
-        )
-      )
-      .digest();
-    logger.debug("Test Typescript Base64 Hash: ", testSigned.toString("hex"));
-    return testSigned.toString("base64");
-  };
-  logger.debug("testSecureHash() :", testSecureHash());
 
   axios({
     method: "POST",
@@ -82,24 +78,21 @@ export async function onlineCheckout(req: Request, res: Response) {
       "Content-Type": "application/json",
     },
     data: {
-      // "secureHash":
-      //   "ZTllODg5MjMwNGNlNWZjOTRjNDI2ZmZmODA4OTJhNWM5NjM3Mzc3YjYzYzhhNDM4ZTUyYjMxZDJhNGVjMzA2MQ==",
-      "secureHash": testSecureHash(),
+      "secureHash": secureHash,
       "apiClientID": apiClientID,
       "serviceID": serviceID,
-      "notificationURL":
-        "https://webhook.site/bfb17e79-a804-4fd9-9478-e9ad80cb8111",
-      "callBackURLOnSuccess": "google.com",
+      "notificationURL": notificationURL,
+      "callBackURLOnSuccess": callBackURLOnSuccess,
       "billRefNumber": billRefNumber,
-      "sendSTK": true,
-      "pictueURL": null,
-      "format": "json",
+      "sendSTK": sendSTK === "true",
+      "pictueURL": pictureURL ?? null,
+      "format": format,
       "currency": currency,
-      "amountExpected": amount,
+      "amountExpected": amountExpected,
       "billDesc": billDesc,
-      "clientMSISDN": "25472",
+      "clientMSISDN": clientMSISDN,
       "clientIDNumber": clientIDNumber,
-      "clientEmail": "john.doe@example.com",
+      "clientEmail": clientEmail,
       "clientName": clientName,
     },
   })
@@ -107,8 +100,6 @@ export async function onlineCheckout(req: Request, res: Response) {
       res.status(response.status).json(response.data);
     })
     .catch((error: any) => {
-      logger.error(error);
-      logger.info("secureHash:", secureHash);
       res
         .status(error.response?.status || 500)
         .json(error.response?.data || {error: "An unknown error occurred"});
